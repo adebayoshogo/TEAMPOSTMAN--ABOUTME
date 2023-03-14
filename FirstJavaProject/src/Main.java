b2cab788866d7a3b190236925c309ec233bee3e2
adebayoshogo83@gmail.com <adebayoshogo>
2023-03-14 16:39:22 +0100

    Added Java files- Task 14

---
+++ FirstJavaProject/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println("My name is Shogo Adebayo");
+        System.out.println("I am a Software Tester");
+        System.out.println("I am passionate about Tech and Tech programs");
+        System.out.println("This is my first Java Code");
+        System.out.println("Today is March 14, 2023");
+        System.out.println(2023);
+    }
+
+}
